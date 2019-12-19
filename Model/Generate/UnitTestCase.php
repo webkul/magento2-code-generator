0f5e92bd42c5c4de283a1c824ea3e35c376d5c04
ashutosh <ashutosh045@webkul.com>
2019-12-19 15:41:35 +0530

    added unit test case generator

--- Model/Generate/UnitTestCase.php
+++ Model/Generate/UnitTestCase.php
@@ -10,6 +10,12 @@ namespace Webkul\CodeGenerator\Model\Generate;
 
 use Webkul\CodeGenerator\Model\Helper as CodeHelper;
 use Webkul\CodeGenerator\Api\GenerateInterface;
+use Zend\Code\Generator\ClassGenerator;
+use Zend\Code\Generator\DocBlockGenerator;
+use Zend\Code\Generator\DocBlock\Tag;
+use Zend\Code\Generator\MethodGenerator;
+use Zend\Code\Generator\PropertyGenerator;
+use Zend\Code\Generator\ParameterGenerator;
 
 /**
  * Class UnitTestCase
@@ -18,6 +24,8 @@ class UnitTestCase implements GenerateInterface
 {
 
     protected $helper;
+
+    protected $docblock;
 
     public function __construct(
         CodeHelper $helper
@@ -31,14 +39,14 @@ class UnitTestCase implements GenerateInterface
     public function execute($data)
     {
         $path = $data['path'];
-        
+        $this->docblock = $this->helper->getHeadDocBlock($data['module']);
         CodeHelper::createDirectory(
-            $unitTestRootDir = $path.DIRECTORY_SEPARATOR.'Test'
+            $unitTestRootDir = $path.DIRECTORY_SEPARATOR.'Test'.DIRECTORY_SEPARATOR.'Unit'
         );
        
-        //$this->generateUnitTest($unitTestRootDir, $data);
+        $this->generateUnitTest($unitTestRootDir, $data);
        
-        return ['status' => 'success', 'message' => "currently under development"];
+        return ['status' => 'success', 'message' => "unit test cases successfully generated"];
     }
 
     /**
@@ -79,9 +87,83 @@ class UnitTestCase implements GenerateInterface
                 }
             }
         }
-       // print_r($methods); echo "\n";
-        
-        // print_r($files);
+
+        $this->generateTestCases($methods, $modulePath);
+    }
+
+    /**
+     * generate unit test cases
+     *
+     * @param [] $methods
+     * @param string $path
+     * @return void
+     */
+    public function generateTestCases($methods, $path)
+    {
+        foreach ($methods as $c => $method) {
+            $pathParts = explode("\\", $c);
+            $vendor = $pathParts[0];
+            $module = $pathParts[1];
+            unset($pathParts[0]);
+            unset($pathParts[1]);
+
+            $fileName = end($pathParts).'Test';
+            unset($pathParts[count($pathParts)+1]);
+           
+            $unitTestPath = implode(DIRECTORY_SEPARATOR, $pathParts);
+            CodeHelper::createDirectory(
+                $path.DIRECTORY_SEPARATOR.'Test'.DIRECTORY_SEPARATOR.'Unit'.DIRECTORY_SEPARATOR.$unitTestPath
+            );
+            $nameSpace = $vendor."\\".$module."\\"."Test\\Unit\\".implode("\\", $pathParts);
+            $classCode = $this->generateTestClass($nameSpace, $fileName);
+            $this->helper->saveFile(
+                $path.DIRECTORY_SEPARATOR.'Test'.DIRECTORY_SEPARATOR.'Unit'.DIRECTORY_SEPARATOR.$unitTestPath.DIRECTORY_SEPARATOR.$fileName.'.php',
+                $classCode->generate()
+            );
+        }
+    }
+
+    /**
+     * genearte class
+     *
+     * @return void
+     */
+    public function generateTestClass($nameSpace, $className)
+    {
+        $unitTestClass      = new ClassGenerator();
+        $generatorsMethods = [
+            MethodGenerator::fromArray([
+                'name'       => 'setUp',
+                'parameters' => [],
+                'visibility' => PropertyGenerator::FLAG_PROTECTED,
+                'body'       => '$this->objectManager = new \\Magento\\Framework\\TestFramework\\Unit\\Helper\\ObjectManager($this);',
+                'docblock'   => DocBlockGenerator::fromArray([
+                    'shortDescription' => 'setup mocks',
+                    'longDescription'  => null,
+                    'tags'             => [
+                        new Tag\ReturnTag([
+                            'datatype'  => 'void',
+                        ]),
+                    ],
+                ]),
+            ])
+        ];
+
+        $unitTestClass->setName($className)
+        ->setNameSpaceName($nameSpace)
+        ->setDocblock(DocBlockGenerator::fromArray([
+            'shortDescription' => $className.' Class',
+        ]))
+        ->addProperties([
+            ['objectManager', '', PropertyGenerator::FLAG_PROTECTED]
+        ])
+        ->setExtendedClass(\PHPUnit\Framework\TestCase::class)
+        ->addMethods($generatorsMethods);
+        $file = new \Zend\Code\Generator\FileGenerator([
+            'classes'  => [$unitTestClass],
+            'docblock' => $this->docblock
+        ]);
+        return $file;
     }
 
     public function getDirContents($dir, &$results = [])
